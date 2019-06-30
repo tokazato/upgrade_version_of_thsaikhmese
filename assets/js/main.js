145dc28cba07b096507762553b08db4b2bd0a6f0
tokazato <tokazato1992>
2019-06-30 16:17:31 +0400

    step 11

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,37 @@ document.addEventListener("DOMContentLoaded", function() {
       // Possibly fall back to a more compatible method here
     }
   });
+
+
+// ------------------- Add change Added ------------------
+
+let add_buttons = document.getElementsByClassName("add");
+let btn = add_buttons.length;
+for (let i = 0; i < btn; i++) {
+  add_buttons[i].addEventListener('click', function() {
+    this.classList.toggle('added');
+      if(this.classList.contains('added')) {
+        this.textContent = 'დამატებულია';
+      } else {
+        this.textContent = 'დამატება';
+      }
+  });
+}
+
+// ------------------- changed Burger with X ------------------
+
+function myFunction(x) {
+  x.classList.toggle("change");
+ }
+
+
+// ------------------- open nav by burger ------------------
+
+const brg = document.querySelector('.burger');
+const menu = document.getElementById('navi');
+
+brg.addEventListener('click', burgerClick);
+
+function burgerClick() {
+  menu.classList.toggle('open');
+}
